a339fd0f4b950cfe49b90524c085e85f8b0df043
H01001000 <heiheiho000@gmail.com>
2023-07-21 18:14:43 -0700

    feat: auto move file

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import fs from 'node:fs/promises'
+
+const audioRecording = await fs.readdir('./WORSHIP/Recording/Audio')
+const videoRecording = await fs.readdir('./WORSHIP/Recording/Video')
+const powerpoint = await fs.readdir('./WORSHIP/Powerpoint/old')
+
+const isSunday = (dateString: string) => {
+  const date = new Date()
+  date.setFullYear(parseInt(`20${dateString.slice(0, 2)}`))
+  date.setMonth(parseInt(dateString.slice(2, 4)) - 1)
+  date.setDate(parseInt(dateString.slice(4, 6)))
+  return date.getDay() === 0
+}
+
+audioRecording.map(async (file) => {
+  const date = file.slice(2, -4)
+  if (!isSunday(date)) return
+  await fs.mkdir(`./WORSHIP/Archive/${date}_Sunday Service/Recording`, { recursive: true })
+  await fs.rename(`./WORSHIP/Recording/Audio/${file}`, `./WORSHIP/Archive/${date}_Sunday Service/Recording/${file}`)
+})
+
+videoRecording.map(async (file) => {
+  const date = file.split(' ')[1].split('-').join('').split('.')[0].slice(2)
+  if (!(/^\d{6}$/gm.test(date))) return
+  if (!isSunday(date)) return
+  await fs.mkdir(`./WORSHIP/Archive/${date}_Sunday Service/Recording`, { recursive: true })
+  fs.rename(`./WORSHIP/Recording/Video/${file}`, `./WORSHIP/Archive/${date}_Sunday Service/Recording/${file}`)
+})
+
+powerpoint.map(async (file) => {
+  const date = file.split('_')[0]
+  if (!(/^\d{6}$/gm.test(date))) return
+  if (!isSunday(date)) return
+  await fs.mkdir(`./WORSHIP/Archive/${date}_Sunday Service`, { recursive: true })
+  await fs.rename(`./WORSHIP/Powerpoint/old/${file}`, `./WORSHIP/Archive/${date}_Sunday Service/${file}`)
+})
